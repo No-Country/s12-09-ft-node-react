eee2ccbd17c19105956a1b1c62835af5a7335b09
Diego Castillo <92400440+Dac1977@users.noreply.github.com>
2023-11-29 10:46:06 -0300

    modelo user

---
+++ backend/src/models/User.ts
@@ -0,0 +1,71 @@
+import { Table,
+	Column,
+	Model,
+	DataType,
+    BeforeCreate,
+    BeforeUpdate
+
+	} from 'sequelize-typescript'
+
+    import * as bcrypt from 'bcrypt'
+
+@Table({
+	timestamps: false,
+	tableName: 'user',
+})
+
+export class Users extends Model {
+	@Column({
+		primaryKey: true,
+		type: DataType.UUID,
+		defaultValue: DataType.UUIDV4,
+        allowNull: false,
+	})
+	id!: string
+    
+	@Column({
+		type: DataType.STRING,
+		allowNull: false,
+	})
+	lastName!: string
+
+	@Column({
+		type: DataType.STRING,
+		allowNull: false,
+	})
+	name!: string
+    
+    @Column({
+		type: DataType.STRING,
+		allowNull: false,
+        unique: true,
+	})
+	email!: string
+
+    @Column({
+		type: DataType.NUMBER,
+		allowNull: false,
+        unique: true,
+	})
+	phone!: string
+
+    @Column({
+		type: DataType.STRING,
+		allowNull: false,
+	})
+	pass!: string
+
+    public async comparePass(candidatePass: string): Promise<boolean> {
+    return bcrypt.compare(candidatePass, this.pass);
+  }
+
+  @BeforeCreate
+  @BeforeUpdate
+  static async hashPass(user: Users) {
+    if (user.changed('pass')) {
+      const saltRounds = 10; // Número de rondas de sal (ajustable según la necesidad)
+      user.pass = await bcrypt.hash(user.pass, saltRounds);
+    }
+  }
+  
+}
