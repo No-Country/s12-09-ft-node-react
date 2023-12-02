33a1996f0d568e62e6e838f52a1167a491135bf3
Yordan <yordanpz@hotmail.com>
2023-12-02 16:49:25 -0300

    users extract password from get request

--- backend/src/models/Users.ts
+++ backend/src/models/Users.ts
@@ -1,33 +1,31 @@
-import { Table,
+import {
+	BeforeCreate,
+	BeforeUpdate,
 	Column,
-	Model,
 	DataType,
-    BeforeCreate,
-    BeforeUpdate
+	Model,
+	Table,
+} from 'sequelize-typescript'
 
-	} from 'sequelize-typescript'
-
-    import * as bcrypt from 'bcrypt'
+import * as bcrypt from 'bcrypt'
 
 @Table({
 	timestamps: false,
 	tableName: 'user',
 })
-
 export class Users extends Model {
-
 	public static validateEmail(email: string): boolean {
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    return emailRegex.test(email);
-  }
+		const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+		return emailRegex.test(email)
+	}
 	@Column({
 		primaryKey: true,
 		type: DataType.UUID,
 		defaultValue: DataType.UUIDV4,
-        allowNull: false,
+		allowNull: false,
 	})
 	id!: string
-    
+
 	@Column({
 		type: DataType.STRING,
 		allowNull: false,
@@ -39,55 +37,60 @@ export class Users extends Model {
 		allowNull: false,
 	})
 	firstName!: string
-    
-    @Column({
+
+	@Column({
 		type: DataType.STRING,
 		allowNull: false,
-        unique: true,
+		unique: true,
 		validate: {
-      isEmail: {
-        msg: 'The email format is not valid',
-      },
-      customValidator(value: string) {
-        if (!Users.validateEmail(value)) {
-          throw new Error('The email format is not valid');
-        }
-      },
-    },
+			isEmail: {
+				msg: 'The email format is not valid',
+			},
+			customValidator(value: string) {
+				if (!Users.validateEmail(value)) {
+					throw new Error('The email format is not valid')
+				}
+			},
+		},
 	})
 	email!: string
 
-    @Column({
+	@Column({
 		type: DataType.INTEGER,
 		allowNull: false,
-        unique: true,
+		unique: true,
 	})
 	phone!: string
 
 	@Column({
-	  type: DataType.STRING,
-      allowNull: false,
-      defaultValue: 'user',
+		type: DataType.STRING,
+		allowNull: false,
+		defaultValue: 'user',
 	})
 	rol!: string
 
-    @Column({
+	@Column({
 		type: DataType.STRING,
 		allowNull: false,
 	})
 	pass!: string
 
-    public async comparePass(candidatePass: string): Promise<boolean> {
-    return bcrypt.compare(candidatePass, this.pass);
-  }
+	public async comparePass(candidatePass: string): Promise<boolean> {
+		return bcrypt.compare(candidatePass, this.pass)
+	}
 
-  @BeforeCreate
-  @BeforeUpdate
-  static async hashPass(user: Users) {
-    if (user.changed('pass')) {
-      const saltRounds = 10; // Número de rondas de sal (ajustable según la necesidad)
-      user.pass = await bcrypt.hash(user.pass, saltRounds);
-    }
-  }
-  
+	@BeforeCreate
+	@BeforeUpdate
+	static async hashPass(user: Users) {
+		if (user.changed('pass')) {
+			const saltRounds = 10 // Número de rondas de sal (ajustable según la necesidad)
+			user.pass = await bcrypt.hash(user.pass, saltRounds)
+		}
+	}
+}
+//Elimino la constraseña para que no la traiga en el get
+Users.prototype.toJSON = function () {
+	const values = Object.assign({}, this.get())
+	delete values.password
+	return values
 }
