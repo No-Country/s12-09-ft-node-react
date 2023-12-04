e23d2b39a7c40946ec6076407f38e342a1cc4acd
Araya Alejo <82895574+alejoaraya@users.noreply.github.com>
2023-12-04 15:19:52 -0300

    add formik, yup and sweetalert to mechanic page

--- frontend/src/app/mechanic/page.tsx
+++ frontend/src/app/mechanic/page.tsx
@@ -1,35 +1,144 @@
+'use client';
+
 import { input as Input } from '@/components';
 import { PlusIcon, UserIcon } from '@/assets/icons';
 import Image from 'next/image';
+import { useFormik } from 'formik';
+import * as yup from 'yup';
+import swal from 'sweetalert';
+
+const basicSchema = yup.object().shape({
+  email: yup.string().email('Plesase enter a valid email').required('Required'),
+  fullName: yup.string().required('Required'),
+  dni: yup.number().positive().integer().required('Required'),
+  phone: yup.number().positive().integer().required('Required'),
+  rol: yup.string().required('Required'),
+  password: yup.string().min(5).required('Required'),
+});
 
 export default function MechanicPage() {
+  const { values, errors, touched, handleChange, handleBlur, handleSubmit } =
+    useFormik({
+      initialValues: {
+        email: '',
+        fullName: '',
+        dni: '',
+        phone: '',
+        rol: '',
+        password: '',
+      },
+      validationSchema: basicSchema,
+      onSubmit: values => {
+        console.log(values);
+        swal("Mecanico guardado", '( simulacion no tiene endpoint )', 'success');
+      },
+    });
+
   return (
-    <div className='flex flex-col justify-center items-center m-5 gap-5 [&>div]:mt-5'>
+    <form
+      onSubmit={handleSubmit}
+      autoComplete='off'
+      className='flex flex-col justify-center items-center m-5 gap-5 [&>div]:mt-5'
+    >
       <h1 className='text-3xl font-bold text-secondary'>Tu equipo</h1>
       <h2 className='text-xl text-center text-accent'>
         Crea usuarios para tu equipo de trabajo
       </h2>
       <div className='flex flex-col gap-3'>
         <div className='flex justify-between'>
-            <button className='btn btn-sm btn-circle bg-base-300'><Image src={UserIcon} alt='user icon'/></button>
-            <button className='btn btn-sm btn-circle btn-ghost'><Image src={PlusIcon} alt='plus icon'/></button>
+          <button type='button' className='btn btn-sm btn-circle bg-base-300'>
+            <Image src={UserIcon} alt='user icon' />
+          </button>
+          <button type='button' className='btn btn-sm btn-circle btn-ghost' onClick={() => {swal("Agregar un nuevo mecanico", '( guarda el anterior y crea uno nuevo )', 'info');}}>
+            <Image src={PlusIcon} alt='plus icon' />
+          </button>
         </div>
-        <Input name='email' placeholder='Email' type='text' />
-        <Input name='name' placeholder='Nombre y apellido' type='text' />
+        <Input
+          className={
+            errors.email && touched.email ? 'input-error border-error' : ''
+          }
+          value={values.email}
+          handleBlur={handleBlur}
+          handleChange={handleChange}
+          name='email'
+          placeholder='Email'
+          type='text'
+        />
+
+        <Input
+          className={
+            errors.fullName && touched.fullName
+              ? 'input-error border-error'
+              : ''
+          }
+          value={values.fullName}
+          handleBlur={handleBlur}
+          handleChange={handleChange}
+          name='fullName'
+          placeholder='Nombre y apellido'
+          type='text'
+        />
         <div className='flex flex-row gap-3'>
           <div className='flex-1 flex flex-col gap-3'>
-            <Input name='dni' placeholder='DNI' type='text' />
-            <Input name='phone' placeholder='Teléfono' type='text' />
+            <Input
+              className={
+                errors.dni && touched.dni ? 'input-error border-error' : ''
+              }
+              value={values.dni}
+              handleBlur={handleBlur}
+              handleChange={handleChange}
+              name='dni'
+              placeholder='DNI'
+              type='text'
+            />
+            <Input
+              className={
+                errors.rol && touched.rol ? 'input-error border-error' : ''
+              }
+              value={values.rol}
+              handleBlur={handleBlur}
+              handleChange={handleChange}
+              name='rol'
+              placeholder='Rol'
+              type='text'
+            />
           </div>
           <div className='flex-1  flex flex-col gap-3'>
-            <Input name='rol' placeholder='Rol' type='text' />
-            <Input name='password' placeholder='Contraseña' type='text' />
+            <Input
+              className={
+                errors.phone && touched.phone ? 'input-error border-error' : ''
+              }
+              value={values.phone}
+              handleBlur={handleBlur}
+              handleChange={handleChange}
+              name='phone'
+              placeholder='Teléfono'
+              type='text'
+            />
+            <Input
+              className={
+                errors.password && touched.password
+                  ? 'input-error border-error'
+                  : ''
+              }
+              value={values.password}
+              handleBlur={handleBlur}
+              handleChange={handleChange}
+              name='password'
+              placeholder='Contraseña'
+              type='password'
+            />
           </div>
         </div>
         <div className='flex justify-center mt-6'>
-        <button className='btn w-20 text-lg font-bold btn-primary text-secondary'>Listo</button>
+          <button
+            type='submit'
+            className='btn w-20 text-lg font-bold btn-primary text-secondary'
+          >
+            Listo
+          </button>
         </div>
       </div>
-    </div>
+    </form>
   );
 }
--- frontend/src/components/input.tsx
+++ frontend/src/components/input.tsx
@@ -1,15 +1,37 @@
+import { ChangeEventHandler } from 'react';
+
 interface InputProps {
-    name?:string,
-    placeholder?:string,
-    type?:string,
-    className?:string
+  name?: string;
+  placeholder?: string;
+  type?: string;
+  className?: string;
+  handleChange?: ChangeEventHandler;
+  handleBlur?: ChangeEventHandler;
+  value?: string;
 }
 
-export const input = ({name,placeholder,type, className} : InputProps) => {
+export const input = ({
+  name,
+  placeholder,
+  type,
+  className,
+  handleChange,
+  handleBlur,
+  value,
+}: InputProps) => {
   return (
     <div>
-        {/* <label htmlFor={label}>{label}</label> */}
-        <input  className="input input-bordered w-full rounded-[15px] bg-base-300 border-base-300 " placeholder={placeholder} name={name}  type={type}/>
+      {/* <label htmlFor={label}>{label}</label> */}
+      <input
+        onChange={handleChange}
+        onBlur={handleBlur}
+        value={value}
+        className={`input input-bordered w-full rounded-[15px] bg-base-300 border-base-300 ${className}`}
+        placeholder={placeholder}
+        name={name}
+        type={type}
+      />
+      {/* {(errors.email && touched.email) && <p className='text-error text-xs mt-[-7px]'>{errors.email}</p> } */}
     </div>
-  )
-}
+  );
+};
