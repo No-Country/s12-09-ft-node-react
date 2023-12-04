6e376864ada9081f48bd0a7ca9e6412e0da11ec9
Joaquin Acosta <joaquinacosta397@outlook.com>
2023-12-04 15:08:42 -0300

    workshop component

--- frontend/src/app/mechanic/page.tsx
+++ frontend/src/app/mechanic/page.tsx
@@ -5,14 +5,20 @@ import Image from 'next/image';
 export default function MechanicPage() {
   return (
     <div className='flex flex-col justify-center items-center m-5 gap-5 [&>div]:mt-5'>
-      <h1 className='text-3xl font-bold text-secondary'>Tu equipo</h1>
+      <h1 className='text-center text-3xl font-bold text-secondary'>
+        Tu equipo
+      </h1>
       <h2 className='text-xl text-center text-accent'>
         Crea usuarios para tu equipo de trabajo
       </h2>
       <div className='flex flex-col gap-3'>
         <div className='flex justify-between'>
-            <button className='btn btn-sm btn-circle bg-base-300'><Image src={UserIcon} alt='user icon'/></button>
-            <button className='btn btn-sm btn-circle btn-ghost'><Image src={PlusIcon} alt='plus icon'/></button>
+          <button className='btn btn-sm btn-circle bg-base-300'>
+            <Image src={UserIcon} alt='user icon' />
+          </button>
+          <button className='btn btn-sm btn-circle btn-ghost'>
+            <Image src={PlusIcon} alt='plus icon' />
+          </button>
         </div>
         <Input name='email' placeholder='Email' type='text' />
         <Input name='name' placeholder='Nombre y apellido' type='text' />
@@ -27,7 +33,9 @@ export default function MechanicPage() {
           </div>
         </div>
         <div className='flex justify-center mt-6'>
-        <button className='btn w-20 text-lg font-bold btn-primary text-secondary'>Listo</button>
+          <button className='btn w-20 text-lg font-bold btn-primary text-secondary'>
+            Listo
+          </button>
         </div>
       </div>
     </div>
--- frontend/src/assets/icons/index.ts
+++ frontend/src/assets/icons/index.ts
@@ -1,9 +1,20 @@
-import BarsIcon from './bars.svg'
-import PlusIcon from './plus.svg'
-import UserIcon from './user.svg'
+import BarsIcon from './bars.svg';
+import PlusIcon from './plus.svg';
+import UserIcon from './user.svg';
+
+import GroupIcon from './group.svg';
+import LockIcon from './lock.svg';
+import MailIcon from './mail.svg';
+import PhoneIcon from './phone.svg';
+import AddressIcon from './address.svg';
 
 export {
-    BarsIcon,
-    PlusIcon,
-    UserIcon
-}
+  BarsIcon,
+  PlusIcon,
+  UserIcon,
+  GroupIcon,
+  LockIcon,
+  MailIcon,
+  PhoneIcon,
+  AddressIcon,
+};
