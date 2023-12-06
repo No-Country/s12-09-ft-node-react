893e7f1e0cdc063f2e143e69906a1546bf7f389d
jooseariias <arijoose@gmail.com>
2023-12-06 20:31:49 -0300

    style:

--- frontend/src/app/home/page.tsx
+++ frontend/src/app/home/page.tsx
@@ -6,6 +6,8 @@ import { type CarModel } from '@/model';
 import { useCar } from '@/hook';
 import botonmas from '../../assets/icons/botonmas.svg';
 import RegisterClient from '@/components/RegisterClient';
+import Link from 'next/link';
+
 const VehiclePage = () => {
   const { getAllCars, cars } = useCar();
 
@@ -33,11 +35,10 @@ const VehiclePage = () => {
           onClick={() => {
             setCurrentView('taller');
           }}
-          className={`${
-            currentView === 'taller'
-              ? ' text-secondary font-bold underline'
-              : 'text-secondary font-bold'
-          }`}
+          className={`${currentView === 'taller'
+            ? ' text-secondary font-bold underline'
+            : 'text-secondary font-bold'
+            }`}
         >
           Taller
         </button>
@@ -45,11 +46,10 @@ const VehiclePage = () => {
           onClick={() => {
             setCurrentView('cliente');
           }}
-          className={` ${
-            currentView === 'cliente'
-              ? ' text-secondary font-bold underline'
-              : 'text-secondary font-bold'
-          }`}
+          className={` ${currentView === 'cliente'
+            ? ' text-secondary font-bold underline'
+            : 'text-secondary font-bold'
+            }`}
         >
           Cliente
         </button>
@@ -58,7 +58,7 @@ const VehiclePage = () => {
       <section className=' flex justify-center mt-10'>
         <div className='md:flex md:flex-row md:flex-wrap gap-10 grid grid-cols-2 items-center justify-center w-full'>
           <div className='hidden md:block '>
-            <button onClick={() => {setOpenModal(true)}} className=' flex flex-col justify-center items-center gap-5'>
+            <button onClick={() => { setOpenModal(true) }} className=' flex flex-col justify-center items-center gap-5'>
               <section className='bg-base-300 rounded-3xl w-24 h-24 flex place-content-center'>
                 <Image width={36} height={36} src={PlusIcon} alt='clio 1.2' />
               </section>
@@ -69,17 +69,21 @@ const VehiclePage = () => {
             <p>No hay vehículos disponibles.</p>
           ) : (
             filteredData.map((vehicle: CarModel, index: number) => (
+
               <div
                 key={index}
                 className=' flex flex-col justify-center items-center'
+
               >
-                <section className='bg-base-300 rounded-3xl w-24 h-24 flex justify-center items-center'>
+                <Link href={`/car/${vehicle.id}`}><section className='bg-base-300 hover:shadow-lg cursor-pointer rounded-3xl w-24 h-24 flex justify-center items-center'>
                   <Image
                     className='h-7 w-20'
                     src={vehicle.imageUrl}
                     alt={`Imagen de ${vehicle.brand}`}
+                    width={30}
+                    height={30}
                   />
-                </section>
+                </section></Link>
                 <p className='text-base font-bold'>{vehicle.brand}</p>
                 <p className='text-base font-normal text-accent'>{`${vehicle.user.firstName} ${vehicle.user.lastName}`}</p>
               </div>
@@ -88,13 +92,13 @@ const VehiclePage = () => {
         </div>
       </section>
 
-      <button onClick={() => {setOpenModal(true)}} className=' block md:hidden  fixed  bottom-[5%] left-[48%]'>
-        <div className='flex justify-center'>
-          <Image src={botonmas} alt='boton' />
+      <button onClick={() => { setOpenModal(true) }} className=' block md:hidden    fixed  bottom-[5%] left-[48%]'>
+        <div className='flex justify-center '>
+          <Image className='hover:scale-150' src={botonmas} alt='boton' />
         </div>
       </button>
-      
-      
+
+
       <RegisterClient open={openModal} handleOpen={setOpenModal} />
     </div>
   );
