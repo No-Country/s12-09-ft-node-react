b293d1abd19a5eef7ac654e26b5762915d6ee17a
Luis Angel Salcedo Gavidia <seemc9@gmail.com>
2023-12-13 11:29:28 -0500

    fix(frontend): Fix handle error in HandleRequest

--- frontend/src/utils/handleRequest.ts
+++ frontend/src/utils/handleRequest.ts
@@ -3,5 +3,9 @@ import type { AxiosResponse } from 'axios';
 export async function handleRequest<T>(
   promise: Promise<AxiosResponse<T>>
 ): Promise<T> {
-  return await promise.then(response => response.data).catch(err => err);
+  return await promise
+    .then(response => response.data)
+    .catch(err => {
+      throw err;
+    });
 }
