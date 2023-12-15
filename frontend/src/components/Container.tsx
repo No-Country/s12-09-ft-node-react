e4b8e8cdea9d3e578d8b1ed17cd64a637083fd8e
alvaro <alvarord519@gmail.com>
2023-12-15 11:37:51 -0500

    Fixed index and container components

--- frontend/src/components/Container.tsx
+++ frontend/src/components/Container.tsx
@@ -3,7 +3,7 @@ interface Props {
   children: JSX.Element | string;
   className?: string;
 }
-export function Container({ children = <></>, className = '' }: Props) {
+export function Container({ children, className = '' }: Props) {
   return (
     <div className={`max-w-[420px] relative mx-auto p-4 ${className}`}>
       {children}
