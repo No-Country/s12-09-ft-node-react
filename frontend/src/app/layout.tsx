14b860d7d5a0a814b2fe38a98ef6c4ebc7af4844
alvaro <alvarord519@gmail.com>
2023-12-18 14:47:48 -0500

    Fixed layouted conflict

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -3,11 +3,8 @@ import { Inter } from 'next/font/google';
 import { StoreProvider } from '@/store';
 
 import './globals.css';
-<<<<<<< HEAD
-import { HeaderHome } from '@/components/layout';
-=======
 import { ModalProvider } from '@/modal';
->>>>>>> 651955d99067b887f89130a6d8b01aaafd9ace93
+import { HeaderHome } from '@/components/layout';
 
 const inter = Inter({ subsets: ['latin'] });
 
