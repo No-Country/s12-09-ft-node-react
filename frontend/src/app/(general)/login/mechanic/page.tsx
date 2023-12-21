5c6410c259e11db34c20855ce1aba4674a94a6f7
alvaro <alvarord519@gmail.com>
2023-12-21 00:44:50 -0500

    Final fix :)

--- frontend/src/app/(general)/login/mechanic/page.tsx
+++ frontend/src/app/(general)/login/mechanic/page.tsx
@@ -3,7 +3,7 @@ import { MechanicLogin } from '@/components/login';
 
 export default async function LoginMechanicPage() {
   return (
-    <div className='w-[400px] relative mx-auto'>
+    <div >
       <Title title='¡Bienvenido al equipo de Mecánicos de MechanicAlert!' className='text-center'>
         <Title.Subtitle>Ingresa tu codigo de acceso</Title.Subtitle>
       </Title>
